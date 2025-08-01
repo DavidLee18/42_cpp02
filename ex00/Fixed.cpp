a8f55188b33520e993721fd04fe363c5e3da5121
Jaehyun Lee <jaehylee@student.42gyeongsan.kr>
2025-08-02 04:07:54 +0900

    impl getter/setter-gen, OCF cons/destructors-gen; ex00

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -2,12 +2,4 @@
 
 IMPL_GETTER_SETTER(Fixed, RawBits, int, raw, raw)
 
-// IMPL_CONS0(Fixed, raw(0))
-
-// IMPL_CONS_COPY(Fixed, other, raw)
-
-// IMPL_ASSIGN(Fixed, other, raw)
-
-// IMPL_DES(Fixed)
-
 IMPL_OCF(Fixed, other, raw, 0)
